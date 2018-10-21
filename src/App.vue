f36409236772c9b125adecf469be6954696aeed1
tenzo <2275182981@qq.com>
2018-10-21 23:03:14 +0800

    01

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <header-top></header-top>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import headerTop from './components/header/header'
+export default {
+  name: 'App',
+  components:{
+    headerTop
+  }
+}
+</script>
+
+<style>
+</style>
